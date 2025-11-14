2a71e14b58f48ccac30b534952b95af128d52709
TiagoMadeira <tiagopmadeira@gmail.com>
2025-11-14 15:44:02 +0000

    Fix: health status endpoint now skips authentication

--- app/controllers/api/v1/health_check_controller.rb
+++ app/controllers/api/v1/health_check_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::HealthCheckController < ApplicationController
   rescue_from(Exception) { render head: 503 }
-
+  skip_before_action :authenticate_user!
   def show
     render head: 200
   end
