37ecbc01a96539f9316744fc7f149711357c62f5
TiagoMadeira <tiagopmadeira@gmail.com>
2025-11-14 15:41:44 +0000

    fix: fixed issues with included concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::API
-    include ApiConcerns
-
+    before_action :check_json_request
     before_action :authenticate_user!
+
+    protected
+
+      def check_json_request
+        return if !request_with_body? || request.content_type&.include?("json")
+        render json: { error: I18n.t("api.errors.invalid_content_type") }, status: :not_acceptable
+      end
+
+    private
+
+    def request_with_body?
+      request.post? || request.put? || request.patch?
+    end
 end
